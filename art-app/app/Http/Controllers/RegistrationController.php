dd64c2b742b0968919bf9f4ce9f4e7ac8a12bfc6
qi <qwang431@usc.edu>
2024-04-26 16:06:10 -0700

    registration/login/logout

---
+++ art-app/app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Hash;
+use Auth;
+
+class RegistrationController extends Controller
+{
+    public function index()
+    {
+        return view('registration/index');
+    }
+
+    public function register(Request $request)
+    {
+        $user = new User();
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = Hash::make($request->input('password')); // bcrypt
+        $user->save();
+
+        Auth::login($user);
+        return redirect()->route('profile.index');
+    }
+}
--- art-app/routes/web.php
+++ art-app/routes/web.php
@@ -3,12 +3,29 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArtController;
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\RegistrationController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('main');
 });
 
+Route::get('/register', [RegistrationController::class, 'index'])->name('registration.index');
+Route::post('/register', [RegistrationController::class, 'register'])->name('registration.create');
+
+Route::get('/login', [AuthController::class, 'loginForm'])->name('login'); // intentionally named "login" per "auth" middleware
+Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+
+
 Route::get('/searchArt', [ArtController::class, 'searchArt'])->name('artworks.search');
 Route::get('/searchExhibition', [ArtController::class, 'searchExhibition'])->name('exhibitions.search');
 
 // Route::get('/displayComments', [CommentController::class, 'showForm'])->name('comments.display');
+
+
+// login allows
+// 1)
+Route::middleware(['auth'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+});
